009395604665a6815a49a938629a715f19f809b4
zxcvbinz <davide.lanotte.unix@gmail.com>
2022-01-24 06:02:17 +0100

    started ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dlanotte <dlanotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 08:24:16 by dlanotte          #+#    #+#             */
-/*   Updated: 2022/01/23 08:24:46 by dlanotte         ###   ########.fr       */
+/*   Updated: 2022/01/24 06:01:44 by dlanotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 #include <fstream>
 #include <string>
 
-int main()
+int main(int argc, char **argv)
 {
+	if (argc != 4 || std::strlen(argv[2]) == 0)
+	{
+		std::cout << "[Console] Command Error" <<std::endl;
+		return (0);
+	}
+	std::string out_name = argv[1];
 	return (0);
 }
